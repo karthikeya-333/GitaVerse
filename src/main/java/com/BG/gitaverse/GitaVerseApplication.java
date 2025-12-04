b13e4f915984922ca69519bcf47cb387d9b29727
Karthikeya <karthikeyachanda@gmail.com>
2025-12-04 16:15:55 +0530

    initial commit

---
+++ src/main/java/com/BG/gitaverse/GitaVerseApplication.java
@@ -0,0 +1,13 @@
+package com.BG.gitaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitaVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitaVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BG/gitaverse/GitaVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BG.gitaverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitaVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
